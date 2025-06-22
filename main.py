1486c88bbc5aa322f0bcb0b9279abc01ba62447b
tuteseta <tutezarandon@gmail.com>
2025-06-22 11:39:29 -0300

    Primero

---
+++ main.py
@@ -0,0 +1,18 @@
+def simular_comportamiento(temperatura, estacion, usuario_tipo):
+    """
+    Devuelve el comportamiento del pato según el tipo de usuario, la temperatura del hábitat y la estación del año.
+    """
+    if usuario_tipo != "premium":
+        return "Funcionalidad exclusiva para usuarios premium"
+
+    if temperatura == "fría":
+        return "El pato está nadando para entrar en calor."
+    elif temperatura == "templada":
+        return "El pato emite su sonido característico."
+    elif temperatura == "caliente":
+        if estacion == "verano":
+            return "El pato vuela alto en verano."
+        else:
+            return "El pato vuela bajo."
+
+    return "Condiciones desconocidas"
